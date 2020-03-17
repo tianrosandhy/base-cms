bf1c4539f2839ae449a1ada7545a8ce707a3b9a6
Christian Rosandhy <tianrosandhy@maxsol.id>
2020-03-17 18:17:53 +0800

    wrong timezone

--- modules/Main/Helper/date.php
+++ modules/Main/Helper/date.php
@@ -3,7 +3,7 @@ function redate($date_format, $date_data){
   $timezone = config('app.timezone');
 
   //generate date as GMT+0
-  $date = new DateTime(date($date_format, $date_data), new DateTimeZone('UTC'));
+  $date = new DateTime(date($date_format, $date_data), new DateTimeZone($timezone));
   $date->setTimezone(new DateTimeZone($timezone));
   return $date->format($date_format);
 }
