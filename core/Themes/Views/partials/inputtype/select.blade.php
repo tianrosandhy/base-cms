61fb76ffe77ce341f19dfa430f689207eac2a849
Christian <tianrosandhy@gmail.com>
2020-05-20 23:42:44 +0800

    minor : suipport theme select type datasource

--- core/Themes/Views/partials/inputtype/select.blade.php
+++ core/Themes/Views/partials/inputtype/select.blade.php
@@ -1,4 +1,26 @@
 <div class="form-group custom-form-group">
+    <?php
+    if(LanguageInstance::isActive()){
+        $selected = isset($value[def_lang()]) ? $value[def_lang()] : (isset($param->default) ? $param->default : null);
+    }
+    else{
+        $selected = $value ? $value : (isset($param->default) ? $param->default : null);
+    }
+
+    if(isset($param->source->as_model)){
+        $datasource = new \Core\Main\Http\Repository\CrudRepository($param->source->model);
+        if(isset($param->source->filter)){
+            $datasource = $datasource->filter($param->source->filter);
+        }
+        $source = [];
+        foreach($datasource as $row){
+            $source[$row->{$param->source->key}] = $row->{$param->source->label};
+        }
+    }
+    else{
+        $source = $param->source;
+    }
+    ?>
     <select name="theme[{{ $name }}][]" class="form-control theme-select2" style="width:100%; border:1px solid #ccc;"
         @if(isset($param->attr))
             @foreach($param->attr as $attr => $attrval)
@@ -9,16 +31,8 @@
             @endforeach
         @endif
     >
-        <?php
-        if(LanguageInstance::isActive()){
-            $selected = isset($value[def_lang()]) ? $value[def_lang()] : (isset($param->default) ? $param->default : null);
-        }
-        else{
-            $selected = $value ? $value : (isset($param->default) ? $param->default : null);
-        }
-        ?>
         <option value=""></option>
-        @foreach($param->source as $key => $value)
+        @foreach($source as $key => $value)
             <option value="{{ $key }}" {{ $selected == $key ? 'selected' : '' }}>{{ $value }}</option>
         @endforeach
     </select>    
